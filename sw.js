ec557e883d7f3817482e379dd53e98861e114566
Bikash Acharya <bikashacharya08@gmail.com>
2026-07-14 16:31:12 +0545

    feat: service worker for offline support + PWA packaging

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'birdbook-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/birds_data.js',
+    '/manifest.json',
+    '/images/icon.svg'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    );
+});
+
+self.addEventListener('fetch', e => {
+    const url = new URL(e.request.url);
+    if (url.origin === location.origin) {
+        e.respondWith(
+            caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+                const clone = res.clone();
+                caches.open(CACHE).then(c => c.put(e.request, clone));
+                return res;
+            }))
+        );
+    }
+});
